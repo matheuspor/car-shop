3223acfee3efeb69a57d4882e89ab50984d18cf8
Matheus <matheus.pormanhani@gmail.com>
2022-04-13 07:42:33 -0300

    refactor: validateWithJoi

--- src/controllers/middlewares/validateWithJoi.ts
+++ src/controllers/middlewares/validateWithJoi.ts
@@ -1,13 +1,21 @@
 import { NextFunction, Request, Response } from 'express';
 import { ObjectSchema } from 'joi';
-import ErrorStatus from '../../utils/joiErrorStatus';
+
+enum ErrorStatus {
+  'any.required' = 400,
+  'boolean.base' = 400,
+  'number.greater' = 400,
+  'number.lower' = 400,
+  'string.empty' = 400,
+  'string.min' = 400,
+}
 
 const validateWithJoi = (schema:ObjectSchema) => 
   (req:Request, res:Response, next:NextFunction) => {
     const { error } = schema.validate(req.body);
 
     if (error) {
-      const errorType = error.details[0].type as keyof typeof ErrorStatus;
+      const errorType = error.details[0].type as keyof typeof ErrorStatus; // https://stackoverflow.com/questions/55377365/what-does-keyof-typeof-mean-in-typescript //
       return res.status(ErrorStatus[errorType])
         .send({ message: error.message });
     }
